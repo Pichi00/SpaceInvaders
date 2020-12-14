828a70f73ab35515d5b568a453d7aba41512af8b
Piotr Kołodziejski <piotr.kolodziejski00@hotmail.com>
2020-12-14 15:04:14 +0100

    Dodaj pliki projektów.

---
+++ SpaceInvaders/SpaceInvaders.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+using namespace std;
+using namespace sf;
+
+/*Wymiary okna*/
+const int WindowWidth = 1024;
+const int WindowHeight = 567;
+
+int main()
+{
+    RenderWindow window{ VideoMode(WindowWidth,WindowHeight), "TEST"};
+
+       while (window.isOpen()) {
+        Event event;
+        window.pollEvent(event);
+        if (event.type == Event::Closed) window.close();
+        window.clear();
+        window.display();
+    }
+    return 0;
+}
+
